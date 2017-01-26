c88b5e1292d3ad7850b3bff43a0a415ec99ce59a
ICECUBE\micha_000 <michael.borland@my.simpson.edu>
2017-01-26 16:04:46 -0600

    File struction and files with base html 5 structure implemented

---
+++ Capstone Website Alpha 1.0/public_html/about_magic_forum.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="eng">
+<head>
+	<meta charset="UTF-8">
+	<title>About Magic Forum</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+</head>
+<body>
+<?php include("../resources/load_scripts.php");?>
+</body>
+</html>
