8c90f8fe245d15d2422326979d8133797908b100
DESKTOP-8M7P8QQ\micha <mmborland@gmail.com>
2017-01-26 21:05:10 -0600

    added the rest of the begining pages. need to finish a small menue and heading before bed.

--- Capstone Website Alpha 1.0/public_html/index.php
+++ Capstone Website Alpha 1.0/public_html/index.php
@@ -1,9 +1,45 @@
 <!DOCTYPE html>
-<html>
+<html lang="eng">
 <head>
+	<meta charset="UTF-8">
 	<title>Index</title>
+	<?php include ("../resources/common_scripts_head.php");?>
+
 </head>
 <body>
+<?php include("../resources/magic_navigation.php");?>
+<h1 class="changing_heading" id="default_heading">Welcome to the Magic-Forum Index</h1>
+<small><p>	This Page is meant to be a spot where you can find all the main pages, then discover even deeper into the conversation threads. </p></small>
 
+	<table class="index_table">
+	    <tbody>
+	        <tr>
+	            <td class="color_1"><a href="index.php">Index</a></td>
+	            <td class="color_2">Index Page For Magic-Forum.cards</td>
+	        </tr>
+	        <tr>
+	            <td class="color_1"><a href="about_magic_forum.php">About Magic-Forum</a></td>
+	            <td class="color_2">Any History detail you need to know about Magic-forum.cards</td>
+	        </tr>
+	        <tr>
+	            <td class="color_1"><a href="magic_home.php">Home</a></td>
+	            <td class="color_2">The Home For news and updates on magic-forums.cards</td>
+	        </tr>
+	        <tr>
+	            <td class="color_1"><a href="magic_format_home.php">Competitive Format Forum</a></td>
+	            <td class="color_2"> Where anyone goes to discuss whats going on in competitive Magic/td>
+	        </tr>
+	        <tr>
+	            <td class="color_1"><a href="fnm_discussion_forum.php">FNM Forum</a></td>
+	            <td class="color_2">Where people go to discuss whats going on at their Friday Night Magic events.</td>
+	        </tr>
+	        <tr>
+	            <td class="color_1"><a href="magic_online_forum.php">Magic Online Forum</a></td>
+	            <td class="color_2">For the Magic Player that sits behind a screen, instead of a table </td>
+	        </tr>
+	        </tbody>
+	        </table>
+<?php include("../resources/magic_footer.php");?>
+<?php include("../resources/load_scripts.php");?>
 </body>
 </html>
