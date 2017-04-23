de4b951876e69ad66ad6f491f5b180855d943bc3
ICECUBE\micha_000 <michael.borland@my.simpson.edu>
2017-04-23 04:40:49 -0500

    no message

--- Capstone-Website-Alpha/resources/db_setup.php
+++ Capstone-Website-Alpha/resources/db_setup.php
@@ -6,12 +6,4 @@
   $database = "capstone_db";
   $server   ="capstone-db.cxwm9jltj41i.us-west-2.rds.amazonaws.com:3306/capstone-db";
 
-?> 
-<!--  
-  <?php  
-  $username = "keys";
-  $password = "hello.world";
-  $database = "capstone_db";
-  $server   ="localhost";
-
- ?> -->
+?>
