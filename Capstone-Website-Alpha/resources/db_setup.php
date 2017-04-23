aee6b7bef33734f608846f82cc2f2a4abf01257f
ICECUBE\micha_000 <michael.borland@my.simpson.edu>
2017-04-23 04:35:06 -0500

    test

--- Capstone-Website-Alpha/resources/db_setup.php
+++ Capstone-Website-Alpha/resources/db_setup.php
@@ -1,17 +1,17 @@
 
-<!-- MySQL that works -->
-<!-- <?php
+ <!-- MySQL that works -->
+<?php
   $username = "capstone_Keys_mb";
   $password = "7NuI$FFddA1bOZZ!1zKD";
   $database = "capstone_db";
   $server   ="capstone-db.cxwm9jltj41i.us-west-2.rds.amazonaws.com:3306/capstone-db";
 
 ?> 
- -->
- <?php  
+<!--  
+  <?php  
   $username = "keys";
   $password = "hello.world";
   $database = "capstone_db";
   $server   ="localhost";
 
- ?>
+ ?> -->
